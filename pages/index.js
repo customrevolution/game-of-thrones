7a3d29302580c4d79f0c6d27917fdb7fdb40570a
valeri_karamfilov <valeri.karamfilov.ext@racingpost.com>
2022-08-21 02:44:36 +0300

    configured environment variables from vercel to work

--- pages/index.js
+++ pages/index.js
@@ -18,17 +18,24 @@ export default function Home({ seasons, backgroundImage, title }) {
         </>
     )
 }
-
 export async function getServerSideProps(context) {
     const {
         title,
         tvSeriesInfo: { seasons }
-    } = await axios(process.env.IMDB_API_TITLE)
+    } = await axios(
+        `${process.env.IMDB_API_TITLE}{process.env.IMDB_API_KEY}/${process.env.IMDB_API_TITLE_ID}`
+    )
         .then((response) => (response.data.errorMessage ? titleMock : response.data))
         .catch(() => titleMock)
 
     const seasonsData = await axios
-        .all(seasons.map((season) => axios.get(`${process.env.IMDB_API_SEASONS}${season}`)))
+        .all(
+            seasons.map((season) =>
+                axios.get(
+                    `${process.env.IMDB_API_SEASONS}${process.env.IMDB_API_KEY}/${process.env.IMDB_API_TITLE_ID}/${season}`
+                )
+            )
+        )
         .then((responses) =>
             responses.map((response, index) =>
                 response.data.errorMessage ? seasonsMock[index] : response.data.episodes
@@ -36,7 +43,9 @@ export async function getServerSideProps(context) {
         )
         .catch(() => seasonsMock)
 
-    const backgroundImage = await axios(process.env.IMDB_API_IMAGES)
+    const backgroundImage = await axios(
+        `${process.env.IMDB_API_IMAGES}${process.env.IMDB_API_KEY}/${process.env.IMDB_API_TITLE_ID}/Full`
+    )
         .then((response) => {
             const images = response.data.items ?? imagesMock
             return images[getRandomIndex(images.length)].image
